8b39d511b11c8ae9cd4b7e9eff3de990d11a5e61
Nicholas Pleschko <n.pleschko@gmail.com>
2022-08-27 08:12:44 +0000

    Fix handling of arrays to respond to commands

--- src/nredis/command_handler.rs
+++ src/nredis/command_handler.rs
@@ -1,20 +1,30 @@
-use super::command_types::{self, RESPValue};
+use super::command_types::{self, RESPValue, RESPError};
 
 pub fn handle_command(command: &command_types::RESPValue) -> command_types::RESPValue {
     match command {
-        command_types::RESPValue::String(x) => handle_single_command(x),
+        command_types::RESPValue::String(x) => handle_string(x),
         command_types::RESPValue::Error(_) => todo!(),
         command_types::RESPValue::Array(items) => {
-            RESPValue::Array(items.iter().map(handle_command).collect())
+            let mut iterator = items.iter();
+            let command = match iterator.next().unwrap() {
+                command_types::RESPValue::String(x) => x,
+                _ => {return command_types::RESPValue::Error(RESPError{message: "Command needs to be defined as string".to_string()})}
+            };
+            let arguments: Vec<&RESPValue> = iterator.collect();
+            handle_single_command(command, &arguments)
         }
     }
 }
 
-fn handle_single_command(message: &str) -> command_types::RESPValue {
+fn handle_string(message: &str) -> command_types::RESPValue {
     let mut message_parts = message.split(' ');
     let command = message_parts.next().unwrap();
     let arguments: Vec<&str> = message_parts.collect();
 
+    handle_single_command(command, &[])
+}
+
+fn handle_single_command(command: &str, arguments: &[&RESPValue]) -> command_types::RESPValue {
     match command.to_lowercase().as_str() {
         "echo" => handle_echo(&arguments),
         "ping" => handle_ping(&arguments),
@@ -22,10 +32,15 @@ fn handle_single_command(message: &str) -> command_types::RESPValue {
     }
 }
 
-fn handle_echo(arguments: &[&str]) -> command_types::RESPValue {
-    RESPValue::String(arguments.join(" "))
+fn handle_echo(arguments: &[&RESPValue]) -> command_types::RESPValue {
+    let string_arguments: Vec<&str> = arguments.iter().map(|&x| match x { 
+        command_types::RESPValue::String(y) => y, 
+        _ => ""
+    }).collect();
+
+    RESPValue::String(string_arguments.join(" "))
 }
 
-fn handle_ping(_: &[&str]) -> command_types::RESPValue {
+fn handle_ping(_: &[&RESPValue]) -> command_types::RESPValue {
     RESPValue::String("PONG".to_string())
 }
